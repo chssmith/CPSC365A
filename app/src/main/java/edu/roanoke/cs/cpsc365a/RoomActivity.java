241048453c68d76cce6db3e248e2f8d3daad1620
zocho <connor.ricks@gmail.com>
2017-03-24 14:12:34 -0400

    Nickname & Room activity skeleton logic.

---
+++ app/src/main/java/edu/roanoke/cs/cpsc365a/RoomActivity.java
@@ -0,0 +1,93 @@
+package edu.roanoke.cs.cpsc365a;
+
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class RoomActivity extends AppCompatActivity {
+
+    //--------------------------------------------------
+    //
+    //  Activity Properties
+    //
+    //--------------------------------------------------
+
+    //--------------------------------------------------
+    //
+    //  Activity Core Handling
+    //
+    //--------------------------------------------------
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room);
+    }
+
+    //--------------------------------------------------
+    //
+    //  Click listeners
+    //
+    //--------------------------------------------------
+
+    //  Description:
+    //  The "Enter!" button was pressed. Handles entering a room session to perform a task.
+    //
+    //  Parameters:
+    //  view is a Button object.
+    //
+    //  Possible Errors:
+    //  If the room string is empty or contains anything but numerical characters, informs the user
+    //  of an invalid format.
+    public void handleEnter(View view) {
+        String room =  ((EditText)findViewById(R.id.roomField)).getText().toString();
+        if (!room.isEmpty() && room.matches("[0-9]+")) {
+            enterRoom();
+        }
+        else {
+            informUser("Rooms are numberical");
+        }
+    }
+
+    //--------------------------------------------------
+    //
+    //  Enter! Handling
+    //
+    //--------------------------------------------------
+
+    //  Description:
+    //  Contacts the server, attempting to get information about a room number.
+    //
+    //  Possible Errors:
+    //  No internet connection, invalid room number, invalid room format.
+    private void enterRoom() {
+        System.out.println("ENTERING A ROOM!");
+
+        /*
+        Make request to server, for the given room.
+          Get response.
+              If valid room.
+                  Get the task from the response.
+                  Present the task to the user, passing the room number along.
+              Else
+                  Inform user of the incorrect room.
+        */
+    }
+
+    //  Description:
+    //  Informs the user that the entered nickname is invalid.
+    //
+    //  Parameters:
+    //  message is a String object.
+    private void informUser(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message)
+                .setCancelable(false)
+                .setPositiveButton("OK", null);
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+}
+
