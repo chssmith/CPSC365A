c12d9090875cc04bd09833c2db829460be3a8974
cssmith <chssmith@roanoke.edu>
2017-03-24 13:28:17 -0400

    Revert "Clearing the repository so connor can commit"
    This reverts commit 9b6ce9d367823cf040141e2a0986d65fcc49cf57.

---
+++ app/src/main/java/edu/roanoke/cs/cpsc365a/MenuActivity.java
@@ -0,0 +1,13 @@
+package edu.roanoke.cs.cpsc365a;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
