ded74db90fad44c7271b75ed80fab846c844ec08
zocho <connor.ricks@gmail.com>
2017-04-26 23:41:35 -0400

    Merge Process....

--- app/src/main/java/edu/roanoke/cs/cpsc365a/NicknameActivity.java
+++ app/src/main/java/edu/roanoke/cs/cpsc365a/NicknameActivity.java
@@ -17,7 +17,6 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-
 public class NicknameActivity extends AppCompatActivity {
 
     //--------------------------------------------------
